a30edfe7e76b55de084d1658feef9abf1b1993df
TheIncgi <theincgi@gmail.com>
2019-02-24 05:39:34 -0500

    updated inheritance func

--- src/main/resources/assets/advancedmacros/scripts/morefunc.lua
+++ src/main/resources/assets/advancedmacros/scripts/morefunc.lua
@@ -16,3 +16,62 @@ end
 function math.map(x, iMin, iMax, oMin, oMax)
   return (x - iMin) * (oMax - oMin) / (iMax - iMin) + oMin
 end
+
+
+
+
+local function className()
+  return "UNNAMED_CLASS"
+end
+--inherit( baseClass )
+--class inheritance function
+--baseClass may be nil for creating a class with no parent
+--constructor can be overriden by saving it to a local variable then
+--calling it durring the call to the 'new' function
+--this will instantiate all parent classes for this object
+function advancedMacros.inherit( baseClass )
+  local class = {}
+  local classMeta = { __index = class }
+
+  function class:new( ... )
+    assert(self, "self can not be nil")
+    local obj = self.__instance and self or {__instance = true}
+    
+    setmetatable( obj, getmetatable(obj) or classMeta )
+    
+    --log("Class &a"..self.className().." &fcalling &e&Nsuper",tostring(baseClass and baseClass.className() or nil))
+    
+    if baseClass then
+      baseClass.new(obj, ... )
+    end
+    obj.__instance = nil --cleanup so :new works normal when done
+    return obj
+  end
+
+  if baseClass then
+    setmetatable( class, { __index = baseClass })
+  end
+
+  function class:class()
+    return class
+  end
+  
+  class.className = className --generic
+  
+  function class:super()
+    return baseClass
+  end
+  function class:isA( someClass )
+    assert(type(someClass)=="table", "Argument provided is not a class")
+    local current = class
+    while current do
+      if current == someClass then
+        return true
+      end
+      current = current:super()
+    end
+    return false
+  end
+
+  return class
+end
