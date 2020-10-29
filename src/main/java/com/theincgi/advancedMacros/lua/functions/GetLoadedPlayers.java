900f0f7dadc0b4ff158cc899c0e43cd4428e249a
aanon <aanonime@wp.pl>
2020-10-29 03:02:14 +0100

    exclude bots in getLoadedPlayers

--- src/main/java/com/theincgi/advancedMacros/lua/functions/GetLoadedPlayers.java
+++ src/main/java/com/theincgi/advancedMacros/lua/functions/GetLoadedPlayers.java
@@ -4,22 +4,25 @@ import java.util.List;
 
 import org.luaj.vm2_v3_0_1.LuaTable;
 import org.luaj.vm2_v3_0_1.LuaValue;
-import org.luaj.vm2_v3_0_1.lib.ZeroArgFunction;
+import org.luaj.vm2_v3_0_1.lib.OneArgFunction;
 
 import com.theincgi.advancedMacros.AdvancedMacros;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 
-public class GetLoadedPlayers extends ZeroArgFunction {
+public class GetLoadedPlayers extends OneArgFunction {
 	@Override
-	public LuaValue call() {
+	public LuaValue call(LuaValue opt) {
+		final boolean includeNPC = opt.optboolean(false);
 		LuaTable table = new LuaTable();
 		int i = 1;
+
 		List<EntityPlayer> players = AdvancedMacros.getMinecraft().world.playerEntities;
 		for(int j = 0; j<players.size(); j++) {
 			EntityPlayer ep = players.get(j);
-			table.set(i++, ep.getName());
+			boolean isNotNPC = ep.getGameProfile().getId().version() != 2;
+			if( includeNPC || isNotNPC ) table.set(i++, ep.getName());
 		}
 		return table;
 	}
