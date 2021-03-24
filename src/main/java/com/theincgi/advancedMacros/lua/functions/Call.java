130889f4f689c141750339bd719bae5b9494871c
aanon <aanonime@wp.pl>
2021-03-24 16:48:22 +0100

    runners accept bytecode

--- src/main/java/com/theincgi/advancedMacros/lua/functions/Call.java
+++ src/main/java/com/theincgi/advancedMacros/lua/functions/Call.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 
+import org.luaj.vm2_v3_0_1.Globals;
 import org.luaj.vm2_v3_0_1.LuaError;
 import org.luaj.vm2_v3_0_1.LuaValue;
 import org.luaj.vm2_v3_0_1.Varargs;
@@ -19,21 +20,22 @@ public class Call extends VarArgFunction{
 	@Override
 	public Varargs invoke(Varargs arg0) {
 		try {
-			try{
+			//try{
 				File f = Utils.parseFileLocation(arg0.arg1());
 //				File f = new File(AdvancedMacros.macrosFolder, arg0.arg1().tojstring());
 //				FileReader fr = new FileReader(f);
-				BufferedReader fr = new BufferedReader(
-					new InputStreamReader( new FileInputStream(f), "UTF8")
-				);
+//				BufferedReader fr = new BufferedReader(
+//					new InputStreamReader( new FileInputStream(f), "UTF8")
+//				);
 				
-				LuaValue function = AdvancedMacros.globals.load(fr, f.getAbsolutePath());
+				Globals g = AdvancedMacros.globals;
+				LuaValue function = g.load(new FileInputStream(f), f.getAbsolutePath(), "bt", g);
 				Varargs args = function.invoke(arg0.subargs(2));
 				return args;
-			} catch (UnsupportedEncodingException e){
-				e.printStackTrace();
-				throw new LuaError("Unable to read UTF-8 in: "+arg0.arg1().checkjstring());
-			}
+			//} catch (UnsupportedEncodingException e){
+			//	e.printStackTrace();
+			//	throw new LuaError("Unable to read UTF-8 in: "+arg0.arg1().checkjstring());
+			//}
 		} catch (Exception e) {
 			throw new LuaError(e);
 		}
--- src/main/java/com/theincgi/advancedMacros/lua/functions/RunThread.java
+++ src/main/java/com/theincgi/advancedMacros/lua/functions/RunThread.java
@@ -8,6 +8,7 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 
+import org.luaj.vm2_v3_0_1.Globals;
 import org.luaj.vm2_v3_0_1.LuaError;
 import org.luaj.vm2_v3_0_1.LuaValue;
 import org.luaj.vm2_v3_0_1.Varargs;
@@ -27,10 +28,11 @@ public class RunThread extends VarArgFunction{
 				//new File(AdvancedMacros.macrosFolder, args.arg1().tojstring());
 				File f = Utils.parseFileLocation(args.arg1());
 				//FileReader fr = new FileReader(f);
-				BufferedReader fr = new BufferedReader(
-					new InputStreamReader( new FileInputStream(f), "UTF8")
-				);
-				function = AdvancedMacros.globals.load(fr, f.getAbsolutePath());
+				//BufferedReader fr = new BufferedReader(
+				//	new InputStreamReader( new FileInputStream(f), "UTF8")
+				//);
+				Globals g = AdvancedMacros.globals;
+				function = g.load(new FileInputStream(f), f.getAbsolutePath(), "bt", g);
 			}else if(args.arg1().isfunction()) {
 				function = args.arg1();
 			}else {
@@ -41,9 +43,9 @@ public class RunThread extends VarArgFunction{
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			throw new LuaError("No such file: "+args.arg1().tojstring());
-		} catch (UnsupportedEncodingException e){
-			e.printStackTrace();
-			throw new LuaError("Unable to read UTF-8 in: "+args.arg1().tojstring());
-		}
+		} // catch (UnsupportedEncodingException e){
+		//	e.printStackTrace();
+		//	throw new LuaError("Unable to read UTF-8 in: "+args.arg1().tojstring());
+		//}
 	}
 }
