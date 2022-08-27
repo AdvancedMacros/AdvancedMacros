a2c67bbf6a35eea694f397671253a7686e4a463b
MajsterTynek <aanonime@wp.pl>
2022-08-27 14:00:11 +0200

    fix for mods using server name after connect

--- src/main/java/com/theincgi/advancedMacros/lua/functions/Connect.java
+++ src/main/java/com/theincgi/advancedMacros/lua/functions/Connect.java
@@ -1,7 +1,7 @@
 package com.theincgi.advancedMacros.lua.functions;
 
 import org.luaj.vm2_v3_0_1.LuaValue;
-import org.luaj.vm2_v3_0_1.lib.OneArgFunction;
+import org.luaj.vm2_v3_0_1.lib.TwoArgFunction;
 
 import com.theincgi.advancedMacros.AdvancedMacros;
 import com.theincgi.advancedMacros.misc.CallableTable;
@@ -19,17 +19,18 @@ public class Connect extends CallableTable{
 		super(new String[] {"connect"}, new Op());
 	}
 
-	private static class Op extends OneArgFunction {
+	private static class Op extends TwoArgFunction {
 		@Override
-		public LuaValue call(LuaValue arg) {
+		public LuaValue call(LuaValue ip, LuaValue name) {
 
 			Utils.runOnMCAndWait( () -> {
 				try {
 				Minecraft mc = AdvancedMacros.getMinecraft();
 				if(mc.world != null)
 					Disconnect.disconnect();
-				ServerData sDat = new ServerData(I18n.format("selectServer.defaultName"), "", false);
-				sDat.serverIP = arg.checkjstring();
+				ServerData sDat = new ServerData("", "", false);
+				sDat.serverName = name.optjstring(I18n.format("selectServer.defaultName"));
+				sDat.serverIP = ip.checkjstring();
 
 				GuiMultiplayer mp = new GuiMultiplayer(null);
 
